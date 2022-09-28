e2ced8b345a8a9d5d8fa1aac53e4fb8309d5d9d8
Olawale Abaniwonda <olawalebash2017@gmail.com>
2022-09-28 17:51:45 +0100

    Update AssemblyInfo.cs

--- H++RT/H++RT/Properties/AssemblyInfo.cs
+++ H++RT/H++RT/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KlipRT")]
+[assembly: AssemblyTitle("H++RT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KlipRT")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("H++RT")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
